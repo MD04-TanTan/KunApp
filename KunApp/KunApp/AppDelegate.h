c48b3c53a41aad2ed8fb7a415e7a30fe2a64590c
Tan Tan <hautp@TanTan.local>
2015-09-07 17:22:45 +0700

    Tan_Create App
    7/9 Create

---
+++ KunApp/KunApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KunApp
+//
+//  Created by Tan Tan on 9/7/15.
+//  Copyright (c) 2015 Tan Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KunApp/KunApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KunApp
+//
+//  Created by Tan Tan on 9/7/15.
+//  Copyright (c) 2015 Tan Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
