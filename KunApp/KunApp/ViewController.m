0a8554ec1f282efdb29faa90fc48cba25842c280
Tan Tan <hautp@TanTan.local>
2015-09-07 18:13:47 +0700

    Tan_Update
    9/7: Update Kun Button + Event

--- KunApp/KunApp/ViewController.m
+++ KunApp/KunApp/ViewController.m
@@ -17,11 +17,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self initApp];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) initApp{
+    UIButton * mainButton = [[UIButton alloc] initWithFrame:CGRectMake(20, 200, 50, 50)];
+    mainButton.layer.cornerRadius = 25.0f;
+    [mainButton setBackgroundColor:[UIColor yellowColor]];
+    [mainButton setTitle:@"Kun" forState:UIControlStateNormal];
+    [mainButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [mainButton addTarget:self action:@selector(btnKunTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:mainButton];
+}
+
+
+
+- (IBAction)btnKunTapped :(id)sender{
+    
+    NSLog(@"click");
 }
 
 @end
